72f625b820518adb36922acb6ad4e4c8e3a7e506
woojj10 <woojj10@gmail.com>
2024-06-04 07:31:11 +0900

    ver3

--- main.py
+++ main.py
@@ -7,16 +7,22 @@ import show_stream
 import home
 if __name__ == "__main__":
     st.title('SKN02-1st-5Team')
-    menu = ['Home', '자동자 정비 현황', 'FAQ 현황', 'ERD']
-    choice = st.sidebar.selectbox('메뉴', menu)
+    tab1,tab2, tab3, tab4= st.tabs(["홈", "자동차 정비 현황", 'FAQ', "ERD"])
     
-    if choice == menu[0] :
+    with tab1 :
         home.run_home()
-    elif choice == menu[1]:
-       
+    with tab2:
         data = pd.read_csv('20240603151251___.csv')
         df, ar =  crawler.crawl(data)
         try:
             show_stream.show_streamit(df, ar)
         except ValueError as e:
             print(e)
+    with tab3:
+        pass
+    with tab4:
+        pass
+    #elif choice == menu[3]:
+    #    st.image(" ")
+    #else:
+    #    st.image(" ")
