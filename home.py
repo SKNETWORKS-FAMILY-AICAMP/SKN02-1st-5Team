ba2c49f5138a0bc2ebac98831784cd7e0cdcfe62
woojj5 <34640735+woojj5@users.noreply.github.com>
2024-06-04 15:00:07 +0900

    Update home.py

--- home.py
+++ home.py
@@ -1,5 +1,4 @@
 import streamlit as st
-<<<<<<< HEAD
 from PIL import Image
 import car_register as a
 
@@ -9,12 +8,3 @@ def run_home() :
     img = Image.open('./image.png')
     st.image(img)
     a.car_registration_crawl()
-
-
-    
-=======
-
-def run_home() :
-    st.subheader('홈 화면입니다')
-    st.text('대기화면')
->>>>>>> 72f625b820518adb36922acb6ad4e4c8e3a7e506
